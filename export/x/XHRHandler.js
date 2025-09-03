cd2d25a68c0abf08092eb527c07d02a86b56dc82
sorrowslee <sorrowslee@gmail.com>
2025-09-03 13:33:05 +0800

    docs: add XHR export comments

--- export/x/XHRHandler.js
+++ export/x/XHRHandler.js
@@ -1,8 +1,12 @@
+// 重新導出 BVFramework 中的 XHRHandler
 if (!cc._RF.push(module, "77926JsF5hDL5NbzKF89qZh", "XHRHandler")) {
+  // 取得 BVFramework 的 XHRHandler 物件
   var R = System.get("bvframework").XHRHandler;
+  // 將 XHRHandler 的所有屬性導出
   for (var K in R) {
     module.exports[K] = R[K];
   }
+  // 標記為 ES 模組
   module.exports.__esModule = true;
   cc._RF.pop();
 }
